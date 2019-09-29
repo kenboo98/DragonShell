c9d4a2a6f6e65da900c13193301907d1dc21ed20
kenboo98 <tellambu@ualberta.ca>
2019-09-28 19:34:14 -0600

    Added output redirection

--- dragonshell.cc
+++ dragonshell.cc
@@ -4,8 +4,9 @@
 #include <iostream>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <fcntl.h>
 #include "builtin.h"
-
+#include <algorithm>
 /**
  * @brief Tokenize a string 
  * 
@@ -31,6 +32,11 @@ std::vector<std::string> tokenize(const std::string &str, const char *delim) {
     return tokens;
 }
 
+void removeSpaces(string &s) {
+    string::iterator endPos = remove(s.begin(), s.end(), ' ');
+    s.erase(endPos, s.end());
+}
+
 vector<char *> stringToCharVector(vector<string> &original) {
     vector<char *> charVector;
     for (string s: original) {
@@ -40,6 +46,20 @@ vector<char *> stringToCharVector(vector<string> &original) {
         charVector.push_back(constS);
     }
     return charVector;
+}
+
+void exec(vector<string> tokens, vector<string> &paths) {
+
+    vector<char *> charTokens = stringToCharVector(tokens);
+    execve(tokens[0].c_str(), &charTokens[0], environ);
+
+    for (string path: paths) {
+        vector<string> temp(tokens);
+        temp[0] = path + temp[0];
+        charTokens = stringToCharVector(temp);
+        execve(temp[0].c_str(), &charTokens[0], environ);
+    }
+    cout << "Command not found" << "\n";
 }
 
 void printWelcome() {
@@ -77,6 +97,49 @@ bool builtInCommands(vector<string> &tokens, vector<string> &paths) {
     return false;
 }
 
+int outputRedirection(vector<string> &redirectCommands, vector<string> &paths) {
+
+    int rc = fork();
+    if (rc == 0) {
+        int file_descriptor;
+        removeSpaces(redirectCommands[1]);
+        string filename = redirectCommands[1];
+        if ((file_descriptor = open(filename.c_str(), O_CREAT | O_WRONLY, 0644)) < 0)
+            perror("open failed!");
+        dup2(file_descriptor, STDOUT_FILENO);    // stdin = ffd
+        close(file_descriptor);
+        vector<string> tokens = tokenize(redirectCommands[0], " ");
+        exec(tokens, paths);
+    } else {
+        //parent process
+        int w_status;
+        wait(&w_status);
+        if (WIFEXITED(w_status) != 1) {
+            cout << "Error" << "\n";
+        }
+    }
+
+    return 0;
+}
+
+
+int singleExternalProgram(vector<string> &tokens, vector<string> &paths) {
+    int rc = fork();
+    if (rc == 0) {
+        int result = 0;
+        //child process
+        exec(tokens, paths);
+        return result;
+    } else {
+        //parent process
+        int w_status;
+        wait(&w_status);
+        if (WIFEXITED(w_status) != 1) {
+            cout << "Error" << "\n";
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     // print the string prompt without a newline, before beginning to read
     // tokenize the input, run the command(s), and print the result
@@ -85,46 +148,30 @@ int main(int argc, char **argv) {
     vector<string> paths = {"/usr/bin/", "/bin/"};
 
     while (true) {
-        cout << "dragonshell >";
+        cout << "dragonshell > ";
         string input;
         getline(cin, input);
+
         vector<string> commands;
         commands = tokenize(input, ";");
         for (string command: commands) {
-            vector<string> tokens;
-            tokens = tokenize(command, " ");
-            int parentPid = getpid();
-            if (builtInCommands(tokens, paths)) {
 
+            vector<string> redirects;
+            redirects = tokenize(command, ">");
+            if (redirects.size() > 1) {
+                outputRedirection(redirects, paths);
             } else {
-                int rc = fork();
-                if (rc == 0) {
-                    int result = 0;
-                    //child process
-                    vector<char *> charTokens = stringToCharVector(tokens);
-                    execve(tokens[0].c_str(), &charTokens[0], environ);
+                vector<string> tokens;
+                tokens = tokenize(command, " ");
+                int parentPid = getpid();
+                if (builtInCommands(tokens, paths)) {
 
-                    for (string path: paths) {
-                        vector<string> temp(tokens);
-                        temp[0] = path + temp[0];
-                        charTokens = stringToCharVector(temp);
-                        execve(temp[0].c_str(), &charTokens[0], environ);
-                    }
-                    cout << "Command not found" << "\n";
-                    return result;
                 } else {
-                    //parent process
-                    int w_status;
-                    wait(&w_status);
-                    if (WIFEXITED(w_status) != 1) {
-                        cout << "Error" << "\n";
-                    }
+                    singleExternalProgram(tokens, paths);
                 }
             }
+
         }
-
-
     }
-
 
 }
